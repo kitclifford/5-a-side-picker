8aea489cc7c1931fce42f5739530b4d00af4bbbe
Kit Clifford-Page <kit.clifford.page@gmail.com>
2020-01-05 22:35:45 +0000

    Updates

---
+++ precache-manifest.fe794cc987e78b750e04a2305c427f4d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddea028981402c374ff7832710ef02c2",
+    "url": "/5-a-side-picker/index.html"
+  },
+  {
+    "revision": "a9dce727f87b51887bda",
+    "url": "/5-a-side-picker/static/css/main.17195d97.chunk.css"
+  },
+  {
+    "revision": "8d260282cefc23f3e733",
+    "url": "/5-a-side-picker/static/js/2.f01276a8.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/5-a-side-picker/static/js/2.f01276a8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a9dce727f87b51887bda",
+    "url": "/5-a-side-picker/static/js/main.e08b9d9f.chunk.js"
+  },
+  {
+    "revision": "cdaad1ca650303702346",
+    "url": "/5-a-side-picker/static/js/runtime-main.d0d00014.js"
+  },
+  {
+    "revision": "23f218b1e3951b213cd1b83f29a79e8a",
+    "url": "/5-a-side-picker/static/media/SFSportsNight.23f218b1.ttf"
+  },
+  {
+    "revision": "0058d2b0612cd1bb7e4514f240cd20db",
+    "url": "/5-a-side-picker/static/media/delete-button.0058d2b0.svg"
+  },
+  {
+    "revision": "c75413a19f171401682a64bc85a9f17e",
+    "url": "/5-a-side-picker/static/media/octin-sports-rg.c75413a1.ttf"
+  },
+  {
+    "revision": "050bd8a064a1fee212c0e75b20487105",
+    "url": "/5-a-side-picker/static/media/soccer-ball.050bd8a0.svg"
+  }
+]);
